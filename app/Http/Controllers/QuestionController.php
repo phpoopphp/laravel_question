5e0423f07de54895f1b69f9da5b360726e0892d3
phpoopphp <Anarhesenov44@gmail.com>
2019-10-15 23:01:42 +0400

    Display all questions

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -14,7 +14,8 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        $questions=Question::latest()->paginate(6);
+        return view('questions.index',compact('questions'));
     }
 
     /**
