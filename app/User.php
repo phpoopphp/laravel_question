dce13426e9449540bdc1a2c9a6bb1e51b3d4cd07
phpoopphp <Anarhesenov44@gmail.com>
2019-10-19 22:48:42 +0400

    Question votes css and html created

--- app/User.php
+++ app/User.php
@@ -52,7 +52,7 @@ class User extends Authenticatable
     public function getAvatarAttribute()
     {
         $email =$this->email;
-        $size = 32;
+        $size = 16;
         return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) )  . "&s=" . $size;
     }
 
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,22 +5,38 @@
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center">
-                            <h3>{{$question->title}}</h3>
-                            <div class="ml-auto">
-                                <a class="btn btn-outline-secondary" href="{{route('questions.index')}}">Back
-                                    Questions</a>
+                    <div class="card-body">
+                        <div class="card-title">
+                            <div class="d-flex align-items-center">
+                                <h3>{{$question->title}}</h3>
+                                <div class="ml-auto">
+                                    <a class="btn btn-outline-secondary" href="{{route('questions.index')}}">Back
+                                        Questions</a>
+                                </div>
                             </div>
                         </div>
-
                     </div>
+                    <hr>
 
-                    <div class="card-body">
-                        <p class="lead">
+
+                    <div class="media">
+                        <div class="flex flex-column align-content-around vote-controls">
+                            <a href="#" title="This question is useful" class="vote-up">
+                                <i class="fas fa-caret-up fa-2x"></i>
+                            </a>
+                            <span class="votes-count">1230</span>
+                            <a href="#" title="This question is useful" class="vote-down off">
+                                <i class="fas fa-caret-down fa-2x"></i>
+                            </a>
+                            <a href="#" title="Click to mark as favorite (Click again to undo)"
+                               class="favorite mt-2 favorited">
+                                <i class="fas fa-star fa-2x"></i> <span class="favorites-count">123</span>
+                            </a>
+                        </div>
+                        <div class="media-body pl-4">
                             {!!  $question->body_html!!}
                             @include('questions.user', ['data' => $question])
-                        </p>
+                        </div>
                     </div>
                 </div>
 
@@ -38,6 +54,18 @@
                                 <div class="card-text">
                                     @foreach($question->answers as $answer)
                                         <div class="media">
+                                            <div class="flex flex-column vote-controls">
+                                                <a href="#" title="This question is useful" class="vote-up">
+                                                    <i class="fas fa-caret-up fa-2x"></i>
+                                                </a>
+                                                <span class="votes-count">1230</span>
+                                                <a href="#" title="This question is useful" class="vote-down off">
+                                                    <i class="fas fa-caret-down fa-2x"></i>
+                                                </a>
+                                                <a href="#" title="Mark this answer as best answer" class="vote-accept mt-2 favorited">
+                                                    <i class="fas fa-check fa-2x"></i>
+                                                </a>
+                                            </div>
                                             <div class="media-body">
                                                 <span class="badge badge-primary">{{$loop->index+1}}</span>
                                                 {!! $answer->body_html !!}
